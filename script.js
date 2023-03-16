3ff396370076fe7864e4051023e3d60d92189fa3
ShemeerCoder-01 <118423467+ShemeerCoder-01@users.noreply.github.com>
2023-03-16 13:17:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,14 @@
 function threeSum(arr, target) {
 // write your code here
 	arr.sort(function(a,b){return a-b;});
+	var sum = 0;
 	for (let i = 0; i < (arr.length) - 2 ; i++) {
-		var sum = 0;
-		var count = 0;
-		for (let j = i; j < i + 3 ; j++) {
-			sum += arr[j];
-		}
+		sum = arr[i] + arr[i+1] + arr[i+2];
 		if (sum === target || sum - 1 === target || sum + 1 === target) {
 			return sum;
+		}else{
+			sum = 0;
 		}
-		
 	}
   
 }
